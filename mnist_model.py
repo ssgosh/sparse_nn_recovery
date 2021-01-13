62030439f6d64cea1e5443968296ed16c5561b84
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-01-13 16:45:25 +0530

    WIP: Compute l1 norm for each layer

--- mnist_model.py
+++ mnist_model.py
@@ -24,23 +24,43 @@ class Net(nn.Module):
         # Enforcing sparsity at relu layer makes sense - though it won't
         # enforce collective sparsity like enforcing it on the max-pool layer
         # would.
-        self.max_pool1_l1 = None
+        #
+        # XXX: What about channel-sparsity? That is, entire channel should be
+        # on or off
+        #
+        # There's no pooling after first conv, so just do this
+        self.conv1_l1 = None 
         self.max_pool2_l1 = None
         self.fc1_l1 = None
+        # list of the above three tensors
+        self.all_l1 = []
 
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu(x)
-        # TODO: compute l1 here
+
+        # NOTE: This works regardless of batch size or number of channels
+        self.conv1_l1 = torch.norm(x, 1) / torch.numelem(x)
+        self.all_l1.clear()
+        self.all_l1.append(self.conv1_l1)
+
         x = self.conv2(x)
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
-        # TODO: compute l1 here
+
+        # Compute l1 here
+        self.max_pool2_l1 = torch.norm(x, 1) / torch.numelem(x)
+        self.all_l1.append(self.max_pool2_l1)
+
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = F.relu(x)
-        # TODO: compute l1 here
+
+        # Compute l1 here
+        self.fc1_l1 = torch.norm(x, 1) / torch.numelem(x)
+        self.all_l1.append(self.fc1_l1)
+
         x = self.dropout2(x)
         x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
