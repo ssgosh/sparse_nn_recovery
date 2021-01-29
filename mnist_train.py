0a40e0f6833eb4b7b4db4087b85ede4c130cd83b
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-01-30 02:30:08 +0530

    WIP: Adversarial Training

--- mnist_train.py
+++ mnist_train.py
@@ -11,13 +11,104 @@ from matplotlib.pyplot import imshow
 from PIL import Image
 import numpy as np
 
-from mnist_model import Net
+from mnist_model import ExampleCNNNet
 from mnist_mlp import MLPNet, MLPNet3Layer
 
 def undo_transform(image):
     mean = 0.1307
     std = 0.3081
     return mean + image * std
+
+
+def compute_generator_loss(config, model_all_l1, adv_output, adv_targetG): 
+    lambd = config['lambd']
+    lambd_layers = config['lambd_layers']
+    include_likelihood = config['include_likelihood']
+    include_layer = config['include_layer']
+
+    if include_likelihood:
+        nll_loss = F.nll_loss(adv_output, adv_targetG)
+    else:
+        nll_loss = 0.
+
+    # include l1 penalty only if it's given as true for that layer
+    l1_loss = 0.
+    if include_layer[0]:
+        l1_loss = lambd * (torch.norm(images + 0.5, 1)
+                / torch.numel(images))
+
+    l1_layers = 0.
+    for include, lamb, l1 in zip(include_layer[1:], lambd_layers,
+            model_all_l1):
+        if include:
+            l1_layers += lamb * l1
+
+    loss = nll_loss + l1_loss + l1_layers
+    return loss
+
+# Performs the training steps for the discriminator
+def training_step_discriminator(optD, data, target, adv_data, adv_targetF,
+        model, config):
+    #lambd = config['lambd']
+    #lambd_layers = config['lambd_layers'] #[0.1, 0.1, 0.1]
+
+    # Steps for training model on real data batch
+    # Zero out gradients accumulated in the model's params
+    optD.zero_grad()
+    
+    # Real data output and losses
+    output = model(data)
+    lossDR = F.nll_loss(output, target)
+
+    # Fake data output and losses for discriminator
+    adv_output = model(adv_data)
+    lossDF = F.nll_loss(adv_output, adv_targetF)
+    
+    lossGF = compute_generator_loss(config, model_all_l1, adv_output, adv_targetR)
+
+    # Supervised loss is now for classifying real data correctly, as well
+    # as adversarial data correctly
+    supervised_loss = lossDR + lossDF
+    loss.backward()
+
+    # Step optD, which changes only the model's params
+    optD.step()
+
+
+# Performs the training steps for "generator", which is really the
+# discriminator trained backwards
+def training_step_generator(optG, model, adv_data, adv_targetR):
+    # Zero-out gradients from previous loss computations
+    optG.zero_grad()
+    
+    optG.step()
+
+# Adversarially train a single epoch
+#
+# adversarial_train_loader points to 1k 28x28 tensors
+# These are initialized to randomly generated mnist_transform(N(0, 0.1))
+# continuously modified during training
+def adversarial_train(args, model, device, train_loader,
+        adversarial_train_loader, optD, optG, epoch):
+    model.train()
+    for batch_idx, (data, target), (adv_data, adv_targetR, adv_targetF) in \
+            enumerate(zip(train_loader, adversarial_train_loader)):
+        # Some stupid pytorch things
+        data, target = data.to(device), target.to(device)
+        adv_data, adv_targetR, adv_targetF = adv_data.to(device), \
+                adv_targetR.to(device), adv_targetF.to(device)
+
+        training_step_discriminator(optD, model, data, target, adv_data, adv_targetF)
+        training_step_generator(optG, model, adv_data, adv_targetR):
+
+        if batch_idx % args.log_interval == 0:
+            sys.stdout.write('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            sys.stdout.write('\r')
+            if args.dry_run:
+                break
+    pass
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
@@ -58,7 +149,17 @@ def test(model, device, test_loader):
 
 def main():
     # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser = argparse.ArgumentParser(description='Modified PyTorch MNIST Example')
+
+    # Training mode
+    #
+    # 'adversarial' does adversarial training, with a single loop for
+    # adversarial examples
+    #
+    # 'normal' just does supervised training 
+    parser.add_argument('--train-mode', type=str, default='normal',
+            metavar='MODE',
+            help='Training mode: adversarial or normal')
     parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                         help='input batch size for training (default: 64)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
@@ -152,7 +253,7 @@ def main():
     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
-    #model = Net().to(device)
+    #model = ExampleCNNNet().to(device)
     #model = MLPNet().to(device)
     model = MLPNet3Layer().to(device)
     optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
