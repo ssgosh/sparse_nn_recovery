bccac9617e0826c3e7277f84b249452a7aed0df8
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-02-08 14:38:14 +0530

    With scaling parameter. Trains more easily to around 90-92%

--- max_norm_layer.py
+++ max_norm_layer.py
@@ -12,16 +12,16 @@ import torch.nn as nn
 # v and d are parameters. 
 #
 # Can also independently scale each dimension, with scaling vector 's':
-# max(0, d - ||x - v||_\infty / s)
+# max(0, d - ||(x - v) / s||_\infty)
 #  division is element-wise. When s_i is close to 0, |x_i - v_i| / s is large
 #  very soon, and it gives a tighter squeeze along that dimension.
 class MaxNormLayer(nn.Module):
-    def __init__(self, size_in, size_out):
+    def __init__(self, size_in, size_out, lambd):
         super().__init__()
         d = torch.empty(size_out)
         nn.init.uniform_(d, 0, 10)
         self.d = nn.Parameter(d)
-        print(self.d)
+        #print(self.d)
         v = torch.randn(size_out, size_in)
         #v = torch.tensor([
         #    [ 0, 0, 0, ], 
@@ -30,22 +30,30 @@ class MaxNormLayer(nn.Module):
         #    [ 1, 0, 0. ]])
 
         self.v = nn.Parameter(v)
-        print(self.v)
-        # TODO: Add 's'
-        self.lambd = 0.1
+
+        # Initially keep the scaling equal to 1
+        s = torch.ones(size_out, size_in)
+        self.s = nn.Parameter(s)
+
+        self.lambd = lambd
 
 
     # Return 0 if outside the bounding box, else distance from closest face
     # x is of shape (N, size_in)
     def forward(self, x):
         x = torch.unsqueeze(x, dim=1)
-        mn = torch.norm(x - self.v, float('inf'), dim=-1)
+        mn = torch.norm((x - self.v) / self.s, float('inf'), dim=-1)
+        r = self.d - mn
         zeros = torch.zeros(mn.shape, device=x.device)
-        return torch.max(zeros, self.d - mn)
+        return torch.max(zeros, r)
+        #leaky = 0.1 * r
+        #return torch.max(leaky, r)
 
 
     def get_weight_decay(self):
-        return self.lambd * torch.mean(self.d * self.d) #+ torch.sum(s*s)
+        return self.lambd * (torch.mean(self.d * self.d) +
+                torch.mean(self.s * self.s))
+        #return 0.
 
 
 if __name__ == '__main__':
--- mnist_max_norm_mlp.py
+++ mnist_max_norm_mlp.py
@@ -9,13 +9,24 @@ class MaxNormMLP(nn.Module):
         super(MaxNormMLP, self).__init__()
         self.flatten1 = nn.Flatten()
 
-        self.mn1 = MaxNormLayer(784, 256)
-        #self.fc1 = nn.Linear(256, 256)
+        self.mn1 = MaxNormLayer(784, 512, lambd=0.01)
+        self.dropout1 = nn.Dropout(0.25)
 
-        #self.mn2 = MaxNormLayer(256, 128)
-        #self.fc2 = nn.Linear(128, 128)
+        #self.fc1 = nn.Linear(512, 256)
+        #self.relu1 = nn.ReLU()
+        #self.dropout1 = nn.Dropout(0.5)
 
-        #self.mn3 = MaxNormLayer(128, 128)
+        #self.fc2 = nn.Linear(256, 128)
+        #self.relu2 = nn.ReLU()
+        #self.dropout2 = nn.Dropout(0.5)
+
+        self.fc2 = nn.Linear(512, 512)
+        self.mn2 = MaxNormLayer(512, 256, lambd=0.001)
+        self.dropout2 = nn.Dropout(0.5)
+
+        #self.fc3 = nn.Linear(256, 256)
+
+        #self.mn3 = MaxNormLayer(256, 128)
 
         self.output = nn.Linear(256, num_classes)
         self.logsoftmax = nn.LogSoftmax(dim=1)
@@ -30,10 +41,20 @@ class MaxNormMLP(nn.Module):
 
         # First hidden layer
         x = self.mn1(x)
+        x = self.dropout1(x)
+
         #x = self.fc1(x)
+        #x = self.relu1(x)
+        #x = self.dropout1(x)
+
+        #x = self.fc2(x)
+        #x = self.relu2(x)
+        #x = self.dropout2(x)
 
         # Second hidden layer
-        #x = self.mn2(x)
+        x = self.fc2(x)
+        x = self.mn2(x)
+        x = self.dropout2(x)
         #x = self.fc2(x)
 
         # Third hidden layer
