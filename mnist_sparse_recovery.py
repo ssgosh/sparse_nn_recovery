4173b339b6c32aa9a5605f4ee43d5f830f7c9389
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-02-08 18:33:47 +0530

    WIP: Add tensorboard changes to image generation code

--- mnist_sparse_recovery.py
+++ mnist_sparse_recovery.py
@@ -17,11 +17,32 @@ import pathlib
 
 # For experiment management
 import wandb
+from torch.utils.tensorboard import SummaryWriter
 
 from mnist_model import ExampleCNNNet
 from mnist_mlp import MLPNet3Layer
 
 np.set_printoptions(precision = 3)
+
+def setup_tensorboard():
+    writer = SummaryWriter()
+    return writer
+
+def mnist_post_process_image_batch(images):
+    transformed_low, transformed_high = compute_mnist_transform_low_high()
+    copied_images = images.clone().detach()
+    post_process_images(copied_images, mode='low_high',
+            low=transformed_low,
+            high=transformed_high)
+    return copied_images
+
+def add_tensorboard_stuff(label, model, images, losses, global_step):
+    writer.add_images("Unfiltered Images", images, dataformats="NCHW",
+            global_step=global_step)
+    filtered_images = mnist_post_process_image_batch(images)
+    writer.add_images("Filtered Images", filtered_images, dataformats="NCHW",
+            global_step=global_step)
+    writer.add_scalars("losses", losses, global_step=global_step)
 
 def get_class(classname):
     return getattr(sys.modules[__name__], classname)
@@ -217,7 +238,7 @@ def post_process_images(images, mode='mean_median', low=None, high=None):
 
 
 # include_layer: boolean vector of whether to include a layer's l1 penalty
-def recover_image(model, images, targets, num_steps, include_layer,
+def recover_image(model, images, targets, num_steps, include_layer, label,
         include_likelihood=True):
     mnist_zero, mnist_one = compute_mnist_transform_low_high()
     images.requires_grad = True
@@ -230,26 +251,35 @@ def recover_image(model, images, targets, num_steps, include_layer,
     lambd_layers = config.lambd_layers #[0.1, 0.1, 0.1]
     #lambd_layers = [0.01, 0.01, 0.01]
     #lambd2 = 1.
+    losses = {}
     for i in range(1, num_steps+1):
         optimizer.zero_grad()
         output = model(images)
         if include_likelihood:
             nll_loss = F.nll_loss(output, targets)
         else:
-            nll_loss = 0.
+            nll_loss = torch.tensor(0.)
+
+        losses[f"{label}/nll_loss"] = nll_loss.item()
 
         # include l1 penalty only if it's given as true for that layer
-        l1_loss = 0.
+        l1_loss = torch.tensor(0.)
         if include_layer[0]:
             l1_loss = lambd * (torch.norm(images - mnist_zero, 1)
                     / torch.numel(images))
 
-        l1_layers = 0.
-        for include, lamb, l1 in zip(include_layer[1:], lambd_layers,
-                model.all_l1):
-            if include:
-                l1_layers += lamb * l1
+        losses[f"{label}/input_l1_loss"] = l1_loss.item()
 
+        l1_layers = torch.tensor(0.)
+        for idx, (include, lamb, l1) in enumerate(zip(include_layer[1:], lambd_layers,
+                model.all_l1)):
+            if include:
+                layer_loss = lamb * l1
+                l1_layers += layer_loss
+                losses[f"{label}/layer_{idx}_l1_loss"] = layer_loss.item()
+
+        losses[f"{label}/hidden_layers_l1_loss"] = l1_layers.item()
+        losses[f"{label}/all_layers_l1_loss"] = l1_loss.item() + l1_layers.item()
         #l1_layers = sum([ (lamb * l1) for lamb, l1 in zip(lambd_layers,
         #    model.all_l1) ])
         #l2_loss = lambd2 * (torch.norm(images, + 2) ** 2
@@ -257,18 +287,22 @@ def recover_image(model, images, targets, num_steps, include_layer,
 
         loss = nll_loss + l1_loss + l1_layers
         loss.backward()
+        optimizer.step()
+        losses[f"{label}/total_loss"] = loss.item()
         print("Iter: ", i,", Loss: %.3f" % loss.item(),
                 f"Prob of {targets[0]} %.3f" %
                 pow(math.e, output[0][targets[0].item()].item()),
                 "images median, mean, std, min, max: %.3f, %.3f, %.3f, %.3f, %.3f" % (
                 images.median().item(), images.mean().item(), images.std().item(), images.min().item(),
                 images.max().item()))
-        optimizer.step()
+
+        # Do tensorboard things
+        add_tensorboard_stuff(label, model, images, losses, i)
 
     images.requires_grad = False
 
 def recover_and_plot_single_digit():
-    recover_image(model, images, targets, 10000, include_layer[label],
+    recover_image(model, images, targets, 10000, include_layer[label], label,
             include_likelihood=False)
 
 def post_process_images_list(images_list):
@@ -298,6 +332,7 @@ def recover_and_plot_images_varying_penalty(initial_image, include_likelihood,
         images += initial_image  # Use same initial image for each digit
         images_list.append(images)
         recover_image(model, images, targets, num_steps, include_layer[label],
+                label,
                 include_likelihood)
 
     post_processed_images_list = []
@@ -354,7 +389,8 @@ def load_and_plot_images_varying_penalty():
 def recover_and_plot_single_image(initial_image, digit):
     label = "input only"
     targets = torch.tensor([digit])
-    recover_image(model, initial_image, targets, 2000, include_layer[label])
+    recover_image(model, initial_image, targets, 2000, include_layer[label],
+            label)
     show_image(initial_image[0][0])
     post_process_images(initial_image, mode='low_high', low=-0.5, high=2.0)
     show_image(initial_image[0][0])
@@ -372,11 +408,11 @@ def load_model(config):
 run = wandb.init(project='mnist_sparse_recovery')
 config = wandb.config
 
-#config.discriminator_model_class = 'ExampleCNNNet'
-config.discriminator_model_class = 'MLPNet3Layer'
+config.discriminator_model_class = 'ExampleCNNNet'
+#config.discriminator_model_class = 'MLPNet3Layer'
 #config.discriminator_model_file = 'mnist_cnn_adv_normal_init.pt'
-#config.discriminator_model_file = 'mnist_cnn_normal_training.pt'
-config.discriminator_model_file = 'mnist_mlp_3layer_adv_normal_init.pt'
+config.discriminator_model_file = 'mnist_cnn.pt'
+#config.discriminator_model_file = 'mnist_mlp_3layer_adv_normal_init.pt'
 
 # Alternate model configuration
 #wandb.config.discriminator_model_class = 'MLPNet3Layer'
@@ -420,6 +456,8 @@ config.include_likelihood = True
 #config.lambd_layers = [1., 1., 1.] #[0.1, 0.1, 0.1]
 config.lambd = 0.1
 config.lambd_layers = [0.1, 0.1, 0.1]
+
+writer = setup_tensorboard()
 
 #labels.remove("no penalty")
 
