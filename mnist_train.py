2a4f32509ee551b8e90fad18f7e5d2700f351d2e
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-02-09 22:31:35 +0530

    Folder refactor

--- mnist_train.py
+++ mnist_train.py
@@ -11,10 +11,10 @@ from matplotlib.pyplot import imshow
 from PIL import Image
 import numpy as np
 
-from mnist_model import ExampleCNNNet
-from mnist_mlp import MLPNet, MLPNet3Layer
-from mnist_max_norm_mlp import MaxNormMLP
-from infinite_dataloader import InfiniteDataLoader
+from models.mnist_model import ExampleCNNNet
+from models.mnist_mlp import MLPNet, MLPNet3Layer
+from models.mnist_max_norm_mlp import MaxNormMLP
+from utils.infinite_dataloader import InfiniteDataLoader
 from utils.batched_tensor_view_data_loader import BatchedTensorViewDataLoader
 
 def undo_transform(image):
