12cfb1063f2950cf300ca025c76a51308c067a91
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-02-11 00:41:50 +0530

    Minor change

--- scripts/run_multiple_lambdas.py
+++ scripts/run_multiple_lambdas.py
@@ -7,7 +7,7 @@ for i, lambd in enumerate([0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 50.0,
             f'--lambd {lambd} ' \
             f'--run-suffix _lambda_{lambd} ' \
             f'--run-dir image_lambda_runs/{i:0>2d}' \
-            ' --num-steps 10'
+            ' --num-steps 1000'
     print(cmd)
     os.system(cmd)
 
