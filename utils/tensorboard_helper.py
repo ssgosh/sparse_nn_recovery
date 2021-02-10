c4fb0fa5ea7200c33a5e889063e4524852b4851f
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-02-10 11:31:48 +0530

    Add prefix to losses and probs to group them in tensorboard

--- utils/tensorboard_helper.py
+++ utils/tensorboard_helper.py
@@ -65,5 +65,6 @@ class TensorBoardHelper:
         self.add_image_grid(filtered_images, f"{label}/Filtered Images", global_step)
         #self.writer.add_images(f"{label}/Filtered Images", filtered_images, dataformats="NCHW",
         #        global_step=global_step)
-        self.log_dict(label, losses, global_step)
-        self.log_dict(label, probs, global_step)
+        self.log_dict(f"{label}/losses", losses, global_step)
+        self.log_dict(f"{label}/probs", probs, global_step)
+
