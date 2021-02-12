fe3f96eda57c9e22d9ce01e004faefe9258f432e
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-02-12 11:58:03 +0530

    Mega loop in script

--- scripts/run_multiple_lambdas.py
+++ scripts/run_multiple_lambdas.py
@@ -2,31 +2,44 @@ import os
 import sys
 import time
 
-mode = 'single-digit'
-digits = list(range(10))
-#digits = [0]
-#lambdas = [0.1, 0.2]
-lambdas = [0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0]
-penalty_mode = "input only"
-num_steps = 10
-timestamp = time.strftime('%b%d_%H-%M-%S')
-pgd = 'disable-pgd' # or 'enable-pgd'
-folder = f"image_lambda_runs/{mode}/{penalty_mode.replace(' ','_')}/num-steps_{num_steps}/{pgd}/{timestamp}"
-for digit in digits:
-    for i, lambd in enumerate(lambdas):
-        cmd = f'python3 mnist_sparse_recovery.py --mode {mode} ' \
-                f'--digit {digit} ' \
-                f'--penalty-mode "{penalty_mode}" ' \
-                f'--lambd {lambd} ' \
-                f'--run-suffix _lambda_{lambd} ' \
-                f'--run-dir {folder}/{digit}/{i:0>2d}' \
-                f' --num-steps {num_steps}' \
-                f' --{pgd}' \
+def inner_loop(mode, penalty_mode, num_steps, pgd, digits, lambdas, timestamp):
+    folder = f"image_lambda_runs/{mode}/{penalty_mode.replace(' ','_')}/num-steps_{num_steps}/{pgd}/{timestamp}"
+    for digit in digits:
+        for i, lambd in enumerate(lambdas):
+            cmd = f'python3 mnist_sparse_recovery.py --mode {mode} ' \
+                    f'--digit {digit} ' \
+                    f'--penalty-mode "{penalty_mode}" ' \
+                    f'--lambd {lambd} ' \
+                    f'--run-suffix _lambda_{lambd} ' \
+                    f'--run-dir {folder}/{digit}/{i:0>2d}' \
+                    f' --num-steps {num_steps}' \
+                    f' --{pgd}' \
 
-        print(cmd)
-        out = os.system(cmd)
-        if out != 0:
-            print('Encountered error, exiting...')
-            sys.exit(1)
+            print(cmd)
+            out = os.system(cmd)
+            if out != 0:
+                print('Encountered error, exiting...')
+                sys.exit(1)
 
+
+def mega_loop():
+    mode = 'single-digit'
+    digits = list(range(10))
+    #digits = [0]
+    #lambdas = [0.1, 0.2]
+    lambdas = [0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0]
+    penalty_mode = "input only"
+    num_steps = 1000
+    timestamp = time.strftime('%b%d_%H-%M-%S')
+    pgd = 'enable-pgd' # or 'enable-pgd'
+
+
+    for penalty_mode in ["input only", "all layers"]:
+        for pgd in ['enable-pgd', 'disable-pgd']:
+            if penalty_mode == 'input only' and pgd == 'disable-pgd':
+                continue
+            print(penalty_mode, pgd)
+            inner_loop(mode, penalty_mode, num_steps, pgd, digits, lambdas, timestamp)
+
+mega_loop()
 
