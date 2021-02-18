b4b9445650a751eb4e21f27eda922ee28d06193a
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-02-18 17:17:26 +0530

    Minor changes to make tests run

--- core/test/test_sparse_input_dataset_recoverer.py
+++ core/test/test_sparse_input_dataset_recoverer.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 class TestSparseInputDatasetRecoverer(TestCase):
     def test_recover_image_dataset(self):
-        #dataset_recoverer = SparseInputDatasetRecoverer(0w)
+        #dataset_recoverer = SparseInputDatasetRecoverer()
         self.fail()
 
 
