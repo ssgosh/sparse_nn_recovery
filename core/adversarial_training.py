6df030bbe56bce9eddc5f925926a99bb820e4cc5
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-02-20 23:50:40 +0530

    WIP: Adversarial Training. Finished most code, now need to test

--- core/adversarial_training.py
+++ core/adversarial_training.py
@@ -1,39 +1,80 @@
 import torch
+import torch.nn.functional as F
 
 from core.sparse_input_dataset_recoverer import SparseInputDatasetRecoverer
 from utils.batched_tensor_view_data_loader import BatchedTensorViewDataLoader
 from utils.infinite_dataloader import InfiniteDataLoader
 
+import sys
+
+
+class ShouldBreak(Exception):
+    pass
+
+
+class TrainLogger():
+    def __init__(self, trainer, log_interval, dry_run):
+        self.trainer = trainer
+        self.log_interval = log_interval
+        self.train_dataset_len = len(trainer.real_data_train_loader)
+        self.dry_run = dry_run
+
+    def log_batch(self, lossval):
+        if self.trainer.next_real_batch % self.log_interval == 0:
+            sys.stdout.write('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                self.epoch, self.trainer.next_real_batch * self.trainer.adv_training_batch_size, self.train_dataset_len,
+                100. * self.trainer.next_real_batch / self.train_dataset_len, lossval)
+            )
+            sys.stdout.write('\r')
+            if self.dry_run:
+                raise ShouldBreak('Breaking because of dry run')
+
+
 
 class AdversarialTrainer:
-    def __init__(self, real_data_train_loader, sparse_input_dataset_recoverer : SparseInputDatasetRecoverer,
-                 model, opt_model, adv_training_batch_size):
-        self.adv_training_batch_size = adv_training_batch_size
+    def __init__(self, real_data_train_loader, sparse_input_dataset_recoverer: SparseInputDatasetRecoverer,
+                 model, opt_model, adv_training_batch_size, device, log_interval, dry_run):
+        self.adv_training_batch_size = adv_training_batch_size  # Same batch size is used for both real and adversarial training
         self.real_data_train_loader = real_data_train_loader
         self.sparse_input_dataset_recoverer = sparse_input_dataset_recoverer
         self.model = model
         self.opt_model = opt_model
         self.next_real_batch = 0
+        self.epoch = 0
+        self.device = device
 
         # Use a fixed iterator to iterate over the training dataset
         self.real_data_train_iterator = iter(self.real_data_train_loader)
 
+        self.logger = TrainLogger(self, log_interval, dry_run)
+
     # Train model on the given batch. Used for real data or adversarial data training
     def train_one_batch(self, batch_inputs, batch_targets):
-        pass
+        data, target = batch_inputs.to(self.device), batch_targets.to(self.device)
+        self.opt_model.zero_grad()
+        output = self.model(data)
+        loss = F.nll_loss(output, target) + self.model.get_weight_decay()
+        loss.backward()
+        self.opt_model.step()
+        self.logger.log_batch(loss.item())
 
     # Train model on only real data for one full epoch. Used for pre-training.
     def train_one_epoch_real(self):
-        pass
+        for self.next_real_batch, real_batch in enumerate(self.real_data_train_loader):
+            real_images, real_targets = real_batch
+            self.train_one_batch(real_images, real_targets)
+        self.epoch += 1
 
-    # Create a batch from real and adversarial data and train
+    # Create a batch from real and adversarial data and call train_one_batch
     def train_one_batch_adversarial(self, real_batch_inputs, real_batch_targets,
                                     adversarial_batch_inputs, adversarial_batch_targets):
-        pass
+        batch_inputs = torch.stack([real_batch_inputs, adversarial_batch_inputs])
+        batch_targets = torch.stack([real_batch_targets, adversarial_batch_targets])
+        self.train_one_batch(batch_inputs, batch_targets)
 
     # One epoch of adversarial training
-    def train_one_epoch_adversarial(self):
-        pass
+    # def train_one_epoch_adversarial(self):
+    #    pass
 
     # Generates m image batches and returns a train loader from it
     # Say adversarial image batch size is 32
@@ -54,20 +95,33 @@ class AdversarialTrainer:
         adversarial_dataset = torch.utils.data.TensorDataset(images,
                                                              targets, fake_class_targets)
         adversarial_train_loader = InfiniteDataLoader(adversarial_dataset,
-                                                      **{'batch_size': self.adv_training_batch_size})
+                                                      **{'batch_size': self.adv_training_batch_size, 'shuffle': True})
         return adversarial_train_loader
 
-
     def generate_m_image_batches_train_one_epoch_adversarial(self, m):
-        pass
+        adversarial_train_loader = self.generate_m_image_batches(m)
+        # Now train
+        self.model.train()
+        # Note that we're using the loader here directly and not the cached iterator.
+        # This creates a new iterator for the for loop, and iterates over all elements from start to end
+        for self.next_real_batch, (real_batch, adv_batch) in enumerate(zip(self.real_data_train_loader, adversarial_train_loader)):
+            real_images, real_targets = real_batch
+            fake_images, _, fake_targets = adv_batch
+            self.train_one_batch_adversarial(real_images, real_targets, fake_images, fake_targets)
+
+        self.epoch += 1
 
     # Say adversarial image batch size is 32
     # and m is 10
     # Then 320 images will be generated
     # Say k is 100.
-    # Then 100 batches of real data and 100 batches each of size 32 from above 320 images will be used for adversarial training
+    # Then 100 batches of real data and 100 batches each of size 32 from above 320 images
+    # will be used for adversarial training
     def generate_m_image_batches_train_k_batches_adversarial(self, m, k):
         adversarial_train_loader = self.generate_m_image_batches(m)
+
+        # Need to set this before training
+        self.model.train()
         # stopped_early keeps track of whether m batches of real data were completed or not before
         # It may happen that we run through the end of the real dataset before we finish m batches.
         # In that case, we'll get another iterator to the loader
@@ -77,10 +131,13 @@ class AdversarialTrainer:
                 real_images, real_targets = real_batch
                 fake_images, _, fake_targets = adv_batch
                 self.train_one_batch_adversarial(real_images, real_targets, fake_images, fake_targets)
-                self.next_real_batch += 1 # Keep track of batch number
+                self.next_real_batch += 1  # Keep track of batch number
                 if i >= m:
                     stopped_early = False
                     break
             if stopped_early:
                 # Get another iterator to the dataset since this one is done
+                # This will shuffle the dataset automatically if it's set in the train loader kwargs
                 self.real_data_train_iterator = iter(self.real_data_train_loader)
+                self.epoch += 1
+                self.next_real_batch = 0
