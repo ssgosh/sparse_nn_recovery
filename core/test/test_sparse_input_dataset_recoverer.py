b78eca2f523f623d8c0b1dacc504734215619d73
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-02-20 16:36:16 +0530

    Modify test for sparse input dataset recoverer

--- core/test/test_sparse_input_dataset_recoverer.py
+++ core/test/test_sparse_input_dataset_recoverer.py
@@ -9,17 +9,25 @@ class TestSparseInputDatasetRecoverer(TestCase):
     def test_recover_image_dataset(self):
         config, include_layer, labels, model, tbh, sparse_input_recoverer = setup_everything(sys.argv[2:])
         sparse_input_recoverer.tensorboard_logging = False
-        dataset_recoverer = SparseInputDatasetRecoverer(sparse_input_recoverer)
-
         n = 20
         bs = 4
-        images, targets = dataset_recoverer.recover_image_dataset_internal(model, output_shape=(n, 1, 28, 28),
-                                                                           num_classes=10, batch_size=bs, num_steps=10,
-                                                                           include_layer=include_layer,
-                                                                           sparsity_mode=config.penalty_mode)
+        dataset_recoverer = SparseInputDatasetRecoverer(
+            sparse_input_recoverer, model, num_recovery_steps=10, batch_size = bs, sparsity_mode=config.penalty_mode,
+            num_classes=10, dataset_len=n, each_entry_shape=(1, 28, 28)
+        )
+
+        images, targets = dataset_recoverer.recover_image_dataset()
 
         assert images.shape == (n, 1, 28, 28)
         assert targets.shape[0] == n
+        print(targets.detach().numpy())
+
+        images, targets = dataset_recoverer.recover_image_dataset_internal(model, output_shape=(100, 1, 28, 28),
+                                                                           num_classes=5, batch_size=10, num_steps=10,
+                                                                           include_layer=include_layer,
+                                                                           sparsity_mode=config.penalty_mode)
+        assert images.shape == (100, 1, 28, 28)
+        assert targets.shape[0] == 100
 
         print(targets.detach().numpy())
         #tbh.add_image_grid(images, "Final Dataset", filtered=False, global_step=None)
