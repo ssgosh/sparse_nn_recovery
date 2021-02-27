a4fdd92e97af9c7bcf8f4bb2756cd2e658b5c712
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-02-28 00:23:21 +0530

    WIP: Add per-class versions of per-epoch aggregate stats

--- core/tblabels.py
+++ core/tblabels.py
@@ -12,11 +12,14 @@ class TBLabels:
       _{train/validation}_{i}:         Adversarial Stats for ith intermittent (adversarial) {train/validation} dataset
       _{train/validation}:            Adversarial Stats averaged over all intermittent datasets; Real data stats
     """
-    # Per-batch. These are the ones we'll look at to see if our model is even training.
+    ########### Per-batch Stats. ##################
+    # These are the ones we'll look at to see if our model is even training. #
     PER_BATCH_ADV_TRAINING_AGGREGATE = "adversarial_training_per_batch_stats_aggregate"
     PER_BATCH_ADV_TRAINING_PER_CLASS = "adversarial_training_per_batch_stats_per_class"
 
-    # Per-epoch. These are the ones that we'll look at to make decisions.
+    ################## Per-epoch Stats ###################################
+    # These are the ones that we'll look at to make decisions. #
+
     # On sample of 5k real training data points, and combined adversarial training data (1k samples each) from all past epochs
     PER_EPOCH_ADV_TRAINING_AGGREGATE_TRAIN = "adversarial_training_per_epoch_stats_aggregate_train"
 
@@ -24,15 +27,47 @@ class TBLabels:
     @staticmethod
     def PER_EPOCH_ADV_TRAINING_AGGREGATE_TRAIN(i): return f"adversarial_training_per_epoch_stats_aggregate_train_{i}"
 
-    # On validation dataset and combined adversarial training data (1k samples each) from all past epochs
-    PER_EPOCH_ADV_TRAINING_AGGREGATE_TRAIN = "adversarial_training_per_epoch_stats_aggregate_train"
+    # On real validation dataset and combined adversarial validation data from all past epochs
+    PER_EPOCH_ADV_VALIDATIONING_AGGREGATE_VALIDATION = "adversarial_training_per_epoch_stats_aggregate_validation"
+
+    # On intermittent adversarial validation datasets
     @staticmethod
     def PER_EPOCH_ADV_TRAINING_AGGREGATE_VALIDATION(i): return f"adversarial_training_per_epoch_stats_aggregate_validation_{i}"
 
+    # On external adversarial validation datasets, such as one-pixel attack, Differential Evolution, our held-out network B
+    @staticmethod
     @staticmethod
     def PER_EPOCH_ADV_TRAINING_AGGREGATE_EXTERNAL_VALIDATION(name): return f"adversarial_training_per_epoch_stats_aggregate_external_validation_{name}"
-    PER_EPOCH_ADV_TRAINING_PER_CLASS = "adversarial_training_per_epoch_stats_per_class"
 
-    # Recovery stats. Internal ones are logged per recovery batch.
-    RECOVERY_INTERNAL = "zzz_recovery_internal"
-    RECOVERY_EPOCH = "recovery_epoch"
+    ####### PER-CLASS versions of above stats ##############
+
+    # Per-epoch. These are the ones that we'll look at to make decisions.
+    # On sample of 5k real training data points, and combined adversarial training data (1k samples each) from all past epochs
+    PER_EPOCH_ADV_TRAINING_PER_CLASS_TRAIN = "adversarial_training_per_epoch_stats_per_class_train"
+
+    # On adversarial training dataset generated in epoch i
+    @staticmethod
+    def PER_EPOCH_ADV_TRAINING_PER_CLASS_TRAIN(i): return f"adversarial_training_per_epoch_stats_per_class_train_{i}"
+
+    # On real validation dataset and combined adversarial validation data from all past epochs
+    PER_EPOCH_ADV_VALIDATIONING_PER_CLASS_VALIDATION = "adversarial_training_per_epoch_stats_per_class_validation"
+
+    # On intermittent adversarial validation datasets
+    @staticmethod
+    def PER_EPOCH_ADV_TRAINING_PER_CLASS_VALIDATION(i): return f"adversarial_training_per_epoch_stats_per_class_validation_{i}"
+
+    # On external adversarial validation datasets, such as one-pixel attack, Differential Evolution, our held-out network B
+    @staticmethod
+    @staticmethod
+    def PER_EPOCH_ADV_TRAINING_PER_CLASS_EXTERNAL_VALIDATION(name): return f"adversarial_training_per_epoch_stats_per_class_external_validation_{name}"
+
+    ####### Recovery stats ########
+
+    # Internal ones are logged per recovery batch.
+    RECOVERY_INTERNAL_AGGREGATE = "zzz_recovery_internal_aggregate"
+    RECOVERY_INTERNAL_PER_CLASS = "zzz_recovery_internal_per_class"
+
+    # recovery_epoch is logged once every epoch on a sample of 100 recovered images
+    RECOVERY_EPOCH_AGGREGATE = "recovery_epoch_aggregate"
+    RECOVERY_EPOCH_PER_CLASS = "recovery_epoch_per_class"
+
