54ce2b32d23ae6bf2c34bcd5a742c80eaf17fe1d
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-03-07 14:34:22 +0530

    Use some contracts in compute_probs_tensor()

--- utils/torchutils.py
+++ utils/torchutils.py
@@ -1,18 +1,19 @@
 import torch
 from torch.nn import functional as F
 
+from icontract import require, ensure
 
+
+# Input contract
+@require(lambda output: len(output.shape) == 2)
+@require(lambda targets: len(targets.shape) == 1)
+@require(lambda output, targets: targets.shape[0] == output.shape[0])
+# Output contract
+@ensure(lambda result, targets: result[0].shape[0] == targets.shape[0])
+@ensure(lambda result, targets: result[1].shape[0] == targets.shape[0])
 def compute_probs_tensor(output, targets):
-    #two_d_indices = torch.cat([torch.arange(targets.shape[0], device=targets.device).unsqueeze(1), targets.unsqueeze(1)], dim=1)
-    #print(two_d_indices)
-    #chunked = two_d_indices.chunk(chunks=3, dim=0)
-    #print(chunked)
     softmax = F.softmax(output, dim=1)
-    #print(targets.shape)
-    #print(output.shape)
-    #print(softmax.shape)
     # For gather(), index must have the same number of dimensions as the input.
     # We'll just repeat target num_classes times along dimension 1
-    targets = torch.cat([targets.unsqueeze(1)]*output.shape[1], dim=1)
-    #print(targets)
+    targets = torch.cat([targets.unsqueeze(1)] * output.shape[1], dim=1)
     return output.gather(1, targets)[:, 0], softmax.gather(1, targets)[:, 0]
