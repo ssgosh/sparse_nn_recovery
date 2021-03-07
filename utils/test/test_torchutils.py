46515e15d5c50e5c078ee6f8a7afa53e0975aa6d
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-03-07 13:37:18 +0530

    WIP: torch.gather()

--- utils/test/test_torchutils.py
+++ utils/test/test_torchutils.py
@@ -12,11 +12,12 @@ from utils.torchutils import compute_probs_tensor
 class TorchUtilsTest(unittest.TestCase):
     def test_compute_probs(self):
         outputs = torch.tensor([
-            [0, 1, 0.],
-            [1, 1, 0],
-            [2, 0, 3]
+            [0, 1, 2.],
+            [3, 4, 5],
+            [2, 7, 8],
+            [9, 10, 11]
         ])
-        targets = torch.tensor([1, 0, 2])
+        targets = torch.tensor([1, 0, 2, 0])
         print(compute_probs_tensor(outputs, targets).detach().numpy())
 
 
--- utils/torchutils.py
+++ utils/torchutils.py
@@ -3,8 +3,16 @@ from torch.nn import functional as F
 
 
 def compute_probs_tensor(output, targets):
-    two_d_indices = torch.cat([torch.arange(targets.shape[0], device=targets.device).unsqueeze(1), targets.unsqueeze(1)], dim=1)
-    print(two_d_indices)
+    #two_d_indices = torch.cat([torch.arange(targets.shape[0], device=targets.device).unsqueeze(1), targets.unsqueeze(1)], dim=1)
+    #print(two_d_indices)
     #chunked = two_d_indices.chunk(chunks=3, dim=0)
     #print(chunked)
-    return F.softmax(output, dim=1).gather(two_d_indices)
+    softmax = F.softmax(output, dim=1)
+    print(targets.shape)
+    print(output.shape)
+    print(softmax.shape)
+    # For gather(), index must have the same number of dimensions as the input.
+    # We'll just repeat target num_classes times along dimension 1
+    targets = torch.cat([targets.unsqueeze(1)]*output.shape[1], dim=1)
+    print(targets)
+    return output.gather(1, targets)
