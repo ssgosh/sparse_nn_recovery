3e4badb21b3a6714d4bcc0703894fd268952950b
Sabyasachi Ghosh <ssgosh@gmail.com>
2021-04-16 16:24:30 +0530

    Generate perturbed dataset images from sparse images

--- perturb_clean_image.py
+++ perturb_clean_image.py
@@ -1,4 +1,7 @@
+import math
+
 import torch
+import torch.nn.functional as F
 import torchvision.transforms
 from matplotlib import pyplot as plot
 
@@ -17,19 +20,38 @@ image, target = dataset[idx]
 
 image = image.permute((1, 2, 0))
 plot.imshow(image, cmap='gray')
-plot.show()
+# plot.show()
 
 d = torch.load('ckpt/attack/images_list.pt')
 sparse_attack_images = d['images'][1]
 attack_targets = d['targets']
 
-attack_image = sparse_attack_images[0]
+attack_image = sparse_attack_images[1]
 attack_image = attack_image.permute((1, 2, 0))
-attack_target = attack_targets[0]
+attack_target = attack_targets[1]
 plot.imshow(attack_image, cmap='gray')
-plot.show()
+# plot.show()
 
-lambd = 0.1
-perturbed_image = torch.clamp(image + lambd * attack_image, 0., 1.)
-plot.imshow(perturbed_image, cmap='gray')
-plot.show()
+class Config:
+    pass
+
+config = Config()
+dataset_helper.setup_config(config)
+config.discriminator_model_file = 'ckpt/mnist_cnn.pt'
+model = dataset_helper.get_model('max-entropy', device='cpu', config=config, load=True)
+
+(mean,), (std,) = dataset_helper.get_mean_std()
+print(mean, std)
+for lambd in [0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 0.8, 0.9, 1.0]:
+    perturbed_image = torch.clamp(image + lambd * attack_image, 0., 1.)
+    batch = perturbed_image.unsqueeze(0)
+    batch = batch.permute((0, 3, 1, 2))  # Channel dimension should be second
+    batch = (batch - mean) / std
+    output = model(batch)
+    print(output)
+    # probs = F.softmax(output)
+    probs = torch.pow(math.e, output)
+    print('lambda = ', lambd)
+    print(probs)
+    plot.imshow(perturbed_image, cmap='gray')
+    plot.show()
